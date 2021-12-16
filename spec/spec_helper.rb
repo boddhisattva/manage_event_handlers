b1c0547dfed838ce899f233eceef0ad47a170846
Mohnish G J <mail4mohnishj@gmail.com>
2021-12-16 12:22:54 +0100

    Require lib files to be used for testing purposes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,12 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+PROJECT_ROOT = File.expand_path('..', __dir__)
+
+Dir.glob(File.join(PROJECT_ROOT, 'lib', '*.rb')).sort.each do |file|
+  require file
+end
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
